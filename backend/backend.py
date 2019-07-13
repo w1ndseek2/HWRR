c67f8c8ab6850925a21394a8993f058fbe80cadd
Frank <frankli0324@hotmail.com>
2019-07-13 21:24:35 +0800

    make some modifications for further changes

--- backend/backend.py
+++ backend/backend.py
@@ -1,45 +1,18 @@
-#coding:utf-8
+# coding:utf-8
 import json
-from flask import Flask, session, request, redirect, jsonify, send_file, render_template
-import pymysql as MySQLdb
+from flask import Flask, session, request, redirect, send_file, render_template
 import ans
 import json
-import coloredlogs
-import logging
-import os
+from utils import (
+    getLoggger,
+    merge_data,
+    verify,
+    execute_sql
+)
 
-MySQLdb.install_as_MySQLdb()
 app = Flask(__name__)
 app.secret_key = 'seasdf'
-conn = MySQLdb.connect(
-    host=os.getenv('DB_URL') or 'localhost',
-    user=os.getenv('DB_USER') or 'username',
-    passwd=os.getenv('DB_PWD') or 'password',
-    db=os.getenv('DB_NAME') or 'test2'
-)
-log = logging.getLogger(__name__)
-coloredlogs.install(
-    level='INFO',
-    logger=log
-)
-
-
-def execute_sql(sql):
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    conn.commit()
-    return cursor.fetchone()
-
-
-def merge_data(data):
-    t = [i['time'] for i in data]
-    x = [i['x'] for i in data]
-    y = [i['y'] for i in data]
-    return [t, x, y]
-
-
-def verify(data):
-    return len(data) == 3 and len(data[0]) >= 3
+log = getLoggger()
 
 
 @app.route('/show_session')
@@ -47,46 +20,92 @@ def ses():
     return json.dumps(dict(session))
 
 
-#前后端连接
+# 注册
+@app.route('/api/register', methods=['POST'])
+def pre_register():
+    username = request.form['username']
+    password = request.form['password']
+    username = username.replace('\"', '')
+    password = password.replace('\"', '')
+    ret = execute_sql(
+        'SELECT username FROM user WHERE username=%(username)s',
+        username=username
+    )
+    if ret is not None and len(ret) > 0:
+        return 'this username has been registered'
+    session['username'] = username
+    session['password'] = password
+    session['action'] = 'register'
+    return redirect('/static/sigpad.html')
+
+
+def register(_data):
+    if session['username'] not in register_data.keys():
+        log.info('设置register_data[session[\'username\']]为空')
+        register_data[session['username']] = []
+    if verify(_data):
+        register_data[session['username']].append(_data)
+        log.info('获取到了一组合法签名数据')
+        log.debug(_data)
+    if len(register_data[session['username']]) == 3:
+        log.info('获取到了三组信息，成功注册')
+        log.debug('所有数据:')
+        log.debug(register_data[session['username']])
+        execute_sql(
+            "INSERT INTO user (id,username,password,sign) values (\
+                NULL, %(username)s, %(password)s, %(sign_data)s\
+            )",
+            username=session['username'],
+            password=session['password'],
+            sign_data=json.dumps(register_data[session["username"]])
+        )
+        register_data.pop(session['username'])
+        session.pop('username')
+        session.pop('password')
+        return 'ret'
+    return 'continue'
+
+
+# 登陆
+@app.route('/api/login', methods=['POST'])
+def pre_login():
+    username = request.form['username']
+    session['username'] = username.replace('\"', '')
+    session['action'] = 'login'
+    return redirect('/static/sigpad.html')
+
+
+def login(_data):
+    true_data = execute_sql(
+        "SELECT sign FROM user WHERE username=%(username)s",
+        username=session['username']
+    )
+    result = ans.match(json.loads(true_data[0]), _data)
+    if result:
+        session['logged_in'] = True
+    return str(result)
+
+
+register_data = {}
+
+
+# 前后端连接
 @app.route('/api/submit', methods=['POST'])
 def submit():
     if 'username' not in session.keys():
-        return 'illeagal request'
+        return 'illegal request'
     data = json.loads(request.get_data())
     _data = []
     for i in data:
         _data += i['points']
     _data = merge_data(_data)
 
-    if 'password' not in session.keys():
-        true_data = execute_sql(
-            f"SELECT sign FROM user WHERE username=\"{session['username']}\";"
-        )
-        result = ans.match(json.loads(true_data[0]), _data)
-        if result:
-            session['logged_in'] = True
-        return str(result)
-
-    if 'data' not in session.keys():
-        log.info('设置session["data"]为空')
-        session['data'] = []
-    if verify(_data):
-        session['data'].append(_data)
-        log.info('获取到了一组合法签名数据')
-        log.debug(session['data'])
-        session['data'] = session['data']
-    if len(session['data']) == 3:
-        log.info('获取到了三组信息，成功注册')
-        log.debug(session['data'])
-        execute_sql(
-            f'UPDATE user SET sign=\"{json.dumps(session["data"])}\" ' +
-            f'WHERE username=\"{session["username"]}\" and password=\"{session["password"]}\"'
-        )
-        session.pop('username')
-        session.pop('password')
-        session.pop('data')
-        return 'ret'
-    return 'continue'
+    if session['action'] == 'login':
+        return login(_data)
+    elif session['action'] == 'register':
+        return register(_data)
+    else:
+        return 'unexpected action'
 
 
 @app.route('/api/logout')
@@ -102,37 +121,21 @@ def index():
         return render_template('guest.html')
     return render_template('user.html', cookies=session)
 
-#干什么的？
-# @app.route('/api/redict')
-# def detail():
-#     return send_file("./frontpage.html")
 
-#注册
-@app.route('/api/register', methods=['POST'])
-def reigster():
+@app.route('/api/update')
+def detail():
     username = request.form['username']
     password = request.form['password']
     username = username.replace('\"', '')
-    password = password.replace('\"', '')
     ret = execute_sql(
-        f'SELECT username FROM user WHERE username=\"{username}\"')
-    if ret is not None and len(ret) > 0:
-        return 'this username has been registered'
-    session['username'] = username
-    session['password'] = password
-    execute_sql(
-        f"insert into user (id,username,password,sign) values (NULL,\"{username}\",\"{password}\",'');"
+        f'SELECT password FROM user WHERE username=\"{username}\"'
     )
-    return redirect('/static/sigpad.html')
+    if ret[0] != password:
+        return render_template('wrong_pass.html')
+    return send_file("./frontpage.html")
 
-#登陆
-@app.route('/api/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    session['username'] = username.replace('\"', '')
-    return redirect('/static/sigpad.html')
 
-#初始化数据库
+# 初始化数据库
 @app.route('/api/db_init', methods=['GET'])
 def db_init():
     init_sqls = [
---
+++ backend/utils.py
@@ -0,0 +1,38 @@
+import logging
+import coloredlogs
+import pymysql
+import os
+pymysql.install_as_MySQLdb()
+conn = pymysql.connect(
+    host=os.getenv('DB_URL') or 'localhost',
+    user=os.getenv('DB_USER') or 'username',
+    passwd=os.getenv('DB_PWD') or 'password',
+    db=os.getenv('DB_NAME') or 'test2'
+)
+
+
+def execute_sql(sql, **kwargs):
+    cursor = conn.cursor()
+    cursor.execute(sql, kwargs)
+    conn.commit()
+    return cursor.fetchone()
+
+
+def merge_data(data):
+    t = [i['time'] for i in data]
+    x = [i['x'] for i in data]
+    y = [i['y'] for i in data]
+    return [t, x, y]
+
+
+def verify(data):
+    return len(data) == 3 and len(data[0]) >= 3
+
+
+def getLoggger(__name__=__name__):
+    log = logging.getLogger(__name__)
+    coloredlogs.install(
+        level='INFO',
+        logger=log
+    )
+    return log
